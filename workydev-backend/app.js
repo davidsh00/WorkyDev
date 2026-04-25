69e3133f131d7f6b0a2e57207a219489af44f6ff
Parham <parham.dadkhah84@gmail.com>
2026-04-25 19:33:04 +0330

    Declared all apis from docs

--- workydev-backend/app.js
+++ workydev-backend/app.js
@@ -3,6 +3,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// APIs about login
+app.get('/api/loginuser',(req,res) =>{
+
+})
+app.update('/api/changepassword',(req,res) =>{
+    
+})
+
+// APIs about the main page
+app.get('/api/dashboardvalues/{user_id}',(req,res) =>{
+    
+})
+app.get('/api/userworks/{user_id}',(req,res) =>{
+    
+
+// APIs about CRUD on work table    
+})
+app.post('/api/insertwork',(req,res) =>{
+    
+})
+app.update('/api/updatework',(req,res) =>{
+    
+})
+app.delete('/api/deletework',(req,res) =>{
+    
+})
+
+
 app.listen(port,() => {
     console.log(`Express app running on port ${port}`);
 })
