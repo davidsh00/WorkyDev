17a05a0e50a7edc6983f2da4771c90b9ba738797
Parham <parham.dadkhah84@gmail.com>
2026-04-26 19:47:56 +0330

    Created the Projects table query in init script

--- workydev-backend/db_init.js
+++ workydev-backend/db_init.js
@@ -6,3 +6,6 @@ const DB = new sqlite.DatabaseSync('worky.db');
 const UsersDDL = DB.prepare('CREATE TABLE IF NOT EXISTS Users (ID INTEGER PRIMARY KEY AUTOINCREMENT, COD_USERNAME TEXT NOT NULL, COD_PASSWORD TEXT NOT NULL, COD_EMAIL TEXT)');
 UsersDDL.run();
 
+// Create the Projects table
+const ProjectsDDL = DB.prepare('CREATE TABLE IF NOT EXISTS Projects (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAM_PROJECT TEXT, USER_ID TEXT NOT NULL, FOREIGN KEY ("USER_ID") REFERENCES "Users"("ID"))');
+ProjectsDDL.run();
