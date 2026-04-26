ebb043180de4633b7a8fda268cd9d0c23ff41233
Parham <parham.dadkhah84@gmail.com>
2026-04-26 19:41:39 +0330

    Created the Users table query in init script

--- workydev-backend/db_init.js
+++ workydev-backend/db_init.js
@@ -1,2 +1,8 @@
 const sqlite = require('node:sqlite');
 const DB = new sqlite.DatabaseSync('worky.db');
+
+
+// Create the Users table
+const UsersDDL = DB.prepare('CREATE TABLE IF NOT EXISTS Users (ID INTEGER PRIMARY KEY AUTOINCREMENT, COD_USERNAME TEXT NOT NULL, COD_PASSWORD TEXT NOT NULL, COD_EMAIL TEXT)');
+UsersDDL.run();
+
