50d9735ab0f00a0fa9fb142fb272cb9170e85014
David.sh <davoodsh000@gmail.com>
2026-04-30 21:20:20 +0330

    refactrot styles

--- workydev-frontend/src/pages/login.jsx
+++ workydev-frontend/src/pages/login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.page}>
+      <div className="page">
         <div className={styles.loginBox}>
           <Image
             className={styles.loginImage}
