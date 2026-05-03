e23f87fbb95438be02cde53db7c6645bdabc2876
Parham <parham.dadkhah84@gmail.com>
2026-05-03 15:00:36 +0330

    Implemented the dashboardvalues method

--- workydev-backend/app.js
+++ workydev-backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { resourceUsage } = require('node:process');
 const sqlite = require('node:sqlite');
 const DB = new sqlite.DatabaseSync('worky.db');
 
@@ -46,9 +45,35 @@ app.post('/api/changepassword',(req,res) =>{
 })
 
 // APIs about the main page
-app.get('/api/dashboardvalues/{user_id}',(req,res) =>{
-    
+app.get('/api/dashboardvalues/:userId',(req,res) =>{
+
+    function duration(startEpoch, endEpoch)
+    { 
+        //TODO: calculate the hours between the two timestamps
+    }
+
+    // to get the parameters specified in the url segment (eg. userId) you use the req.params object
+    const userId = req.params.userId;
+
+    if(!userId) {res.status(400).json({result:"failed",error:"the user id was not specified"});}
+
+    // Get the project_count
+    const project_count_query = DB.prepare('SELECT COUNT(*) AS C FROM Projects WHERE USER_ID = ?');
+    const project_count = project_count_query.all(userId)[0].C; // project_count is an array of rows and the column that contains the count is called 'C'
+
+    // Get work_count
+    const work_count_query = DB.prepare('SELECT COUNT(*) AS C FROM Works WHERE USER_ID = ?');
+    const work_count = project_count_query.all(userId)[0].C;
+
+    // TODO: calculate hours count
+    // // Get hours_count
+    // const hours_count_query = DB.prepare('SELECT * FROM Works WHERE USER_ID = ?');
+    // const hours_count = project_count_query.all(userId);
+
+    res.status(200).json({project_count:project_count,work_count:work_count,hours_count:0});
 })
+
+
 app.get('/api/userworks/{user_id}',(req,res) =>{
     
 
