6ca8b571f6537f6923387356111b64f40bfc656a
Parham <parham.dadkhah84@gmail.com>
2026-05-04 14:32:12 +0330

    Implemented userworks method

--- workydev-backend/app.js
+++ workydev-backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { worker } = require('node:cluster');
 const sqlite = require('node:sqlite');
 const DB = new sqlite.DatabaseSync('worky.db');
 
@@ -74,11 +75,52 @@ app.get('/api/dashboardvalues/:userId',(req,res) =>{
 })
 
 
-app.get('/api/userworks/{user_id}',(req,res) =>{
+app.get('/api/userworks/:userId',(req,res) =>{
     
+    // to get the parameters specified in the url segment (eg. userId) you use the req.params object
+    const userId = req.params.userId;
+    if(!userId) {res.status(400).json({result:"failed",error:"the user id was not specified"});}
 
-// APIs about CRUD on work table    
+    // left join so works without projects are handled aswell
+    const userworks_query = DB.prepare(
+        `SELECT w.id, w.DAT_START, w.DAT_END, p.id AS PROJECT_ID, NAM_PROJECT FROM Works w 
+        LEFT JOIN Workprojects wp ON wp.WORK_ID = w.ID 
+        LEFT JOIN Projects p ON wp.PROJECT_ID = p.ID 
+        WHERE w.USER_ID = ?
+        ORDER BY w.id`);
+    const userworks = userworks_query.all(userId);
+    
+    // to group rows based on their id
+    let userWorksAgg = [];
+    userworks.forEach(row => {
+        let work = {};
+        let workIndex = -1;
+        if(-1 === userWorksAgg.findIndex(elm => elm.id == row.ID))
+        {
+            work = {id:row.ID, dat_start:row.DAT_START, dat_end:row.DAT_END, projects:new Array(0)};
+        }
+        else
+        {
+            workIndex = userWorksAgg.findIndex(elm => elm.id == row.ID); 
+            work = userWorksAgg[workIndex];
+
+            delete userWorksAgg[workIndex];
+        }
+        if(row.PROJECT_ID)
+        {
+            console.log(work);
+            let project = {id:row.PROJECT_ID, nam_project: row.NAM_PROJECT};
+            work.projects.push(project);   
+        }
+        if(workIndex != -1) {userWorksAgg[workIndex] = work}
+        else {userWorksAgg.push(work);}        
+    });
+
+
+    res.status(200).send(userWorksAgg);
 })
+
+// APIs about CRUD on work table
 app.put('/api/insertwork',(req,res) =>{
     
 })
